8f54a7930e3895639c52cafe95f444879a5315ae
HassanAzzam <hassanazzam95@gmail.com>
2019-02-25 01:25:13 +0200

    v1.0

--- background.js
+++ background.js
@@ -4,16 +4,25 @@
 
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('The color is green.');
-  });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
-      })],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
-});
+let checkArabic = ( character ) => {
+  var RTL = ['ا','ب','پ','ت','س','ج','چ','ح','خ','د','ذ','ر','ز','ژ','س','ش','ص','ض','ط','ظ','ع','غ','ف','ق','ک','گ','ل','م','ن','و','ه','ی'];
+  return RTL.indexOf( character ) > -1;
+}
+
+let sheet = (() => {
+let style = document.createElement("style");
+style.appendChild(document.createTextNode(""));
+document.head.appendChild(style);
+return style.sheet;
+})();
+
+let checkPost = () => {
+  let post = document.getElementsByClassName('postArticle-content')[0];
+  if(post && post.textContent && post.textContent.length && checkArabic(post.textContent[0])) {
+     sheet.insertRule(".postArticle-content { direction: rtl !important }");
+     sheet.insertRule(".postArticle-content .u-paddingLeft15 { padding-right: 15px !important }");
+  }
+};
+
+// document.onload = () => { checkPost() };
+setTimeout(checkPost, 100);
